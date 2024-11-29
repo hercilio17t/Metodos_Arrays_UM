e38c2fca925d7978a507077f3f3e88fdfa2d5df1
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-29 13:07:16 -0300

    Exercício 4

--- src/exercicio-04/filaPilha.ts
+++ src/exercicio-04/filaPilha.ts
@@ -1,4 +1,23 @@
-// IMPLEMENTE AQUI A SUA FUNÇÃO
+/*
+Problema
+Você está desenvolvendo um sistema de gerenciamento de pedidos para um restaurante movimentado. 
+Para atender às diferentes necessidades do restaurante, esse sistema precisa suportar o armazenamento dos 
+pedidos dos clientes tanto uma uma fila (primeiro a pedir e o primeiro a ser atendido) 
+quanto em uma pilha (o último a pedir e o primeiro a ser atendido).
+
+Entrada
+A entrada será composta por três variáveis:
+
+pedidoSolicitado: objeto contendo os dados do novo pedido (nome do cliente, nome do prato e quantidade)
+pedidos: array de objetos contendo todos pedidos cadastrados na ordem em que deve ser atendidos
+tipoDeAtendimento: string contendo como o pedido deve ser cadastrado (pilha ou fila)
+Saída
+Seu programa deve RETORNAR o novo array de pedidos.
+
+OBS: Observe que caso o tipo de atendimento seja pilha, o pedido solicitado deve ser inserido na primeira 
+posição do array pedidos (essa pessoa terá prioridade para ser atendida); caso seja fila, deve ser inserido na última posição.
+*/
+
 type TPedido = {
   nomeCliente: string;
   nomePrato: string;
@@ -7,11 +26,15 @@ type TPedido = {
 
 type TAtendimento = "fila" | "pilha";
 
-function solucao(
-  pedidoSolicitado: TPedido,
-  pedidos: TPedido[],
-  tipoDeAtendimento: TAtendimento
-): TPedido[] {
+function solucao(pedidoSolicitado: TPedido, pedidos: TPedido[], tipoDeAtendimento: TAtendimento): TPedido[] {
   // seu código aqui
+  if(tipoDeAtendimento === 'fila'){
+    pedidos.push(pedidoSolicitado)
+  }
+  else{
+    pedidos.unshift(pedidoSolicitado)
+  }
+  return pedidos
+  
 }
 export default solucao;
