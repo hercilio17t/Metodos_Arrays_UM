e734f12e88c4a61db593bdee1281513d2ca873ec
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-29 12:53:02 -0300

    Exercício 3

--- src/exercicio-03/agendamentoPacientes.ts
+++ src/exercicio-03/agendamentoPacientes.ts
@@ -1,4 +1,30 @@
+/*
+Problema
+Você trabalha como desenvolvedor de software para uma clínica médica. 
+No momento, você está desenvolvendo a funcionalidade do sistema responsável pelo cancelamento de agendamentos de consultas.
+
+Entrada
+A entrada será composta por duas variáveis:
+
+agenda: array de strings que armazena os nomes dos pacientes agendados
+cancelamento: string contendo o nome do paciente que deseja cancelar o agendamento
+Saída
+Seu programa deve RETORNAR:
+
+Agenda atualizada sem o nome do paciente que solicitou o cancelamento, caso o paciente tenha sido agendado
+PACIENTE NAO AGENDADO: caso o paciente não tenha sido agendado
+Exemplos
+*/
+
 function solucao(agenda: string[], cancelamento: string): string | string[] {
   // seu código aqui
+  if(agenda.includes(cancelamento)){
+    const novaAgenda: string[] = agenda.filter((pacientes) => {
+      return pacientes != cancelamento 
+    })
+    return novaAgenda
+  }
+  return 'PACIENTE NAO AGENDADO'
+
 }
 export default solucao;
